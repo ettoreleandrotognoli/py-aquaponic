de6a2fc916165a360a88088efaa2f30d98a3b662
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2017-11-13 23:05:09 -0200

    travis cache & coverage & ldr

--- src/iot/fusion/ldr.py
+++ src/iot/fusion/ldr.py
@@ -1,9 +1,6 @@
 from iot.models import MeasureUnit
 from .merger import Merger
 
-lux_unit = MeasureUnit.objects.get(name='lux')
-lumen_unit = MeasureUnit.objects.get(name='lumen')
-
 
 class Lux(Merger):
     def __init__(self, resistor=10000.0, tension=5.0, ldr_constant=500000.0, light_proportional=True):
@@ -11,6 +8,7 @@ class Lux(Merger):
         self.tension = tension
         self.ldr_constant = ldr_constant
         self.light_proportional = light_proportional
+        self.lux_unit = MeasureUnit.objects.get(name='lux')
 
     def merge(self, output_sensor, sensor_data, sensors):
         value = sensor_data.value
@@ -18,15 +16,16 @@ class Lux(Merger):
             lux = (value * self.ldr_constant) / (self.resistor * (self.tension - value))
         else:
             lux = (self.tension * self.ldr_constant - self.ldr_constant) / (value * self.resistor)
-        return lux, sensor_data.time, lux_unit
+        return lux, sensor_data.time, self.lux_unit
 
 
 class Lumen(Lux):
     def __init__(self, ldr_area=0.000025, **kwargs):
         super(Lumen, self).__init__(**kwargs)
         self.ldr_area = ldr_area
+        self.lumen_unit = MeasureUnit.objects.get(name='lumen')
 
     def merge(self, sensor_data, sensors):
         lux, time, unit = super(Lumen, self).merge(sensor_data, sensors)
         lumen = lux * self.ldr_area
-        return lumen, time, lumen_unit
+        return lumen, time, self.lumen_unit
