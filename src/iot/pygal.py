ed7fec7b83b3474dcc286a74f64b207a7499b999
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2017-11-17 12:25:06 -0200

    set style

--- src/iot/pygal.py
+++ src/iot/pygal.py
@@ -1,4 +1,24 @@
+import pydoc
+
 import pygal
+
+allowed_styles = {
+    'default': 'DefaultStyle',
+    'dark': 'DarkStyle',
+    'neon': 'NeonStyle',
+    'dark_solarized': 'DarkSolarizedStyle',
+    'light_solarized': 'LightSolarizedStyle',
+    'light': 'LightStyle',
+    'clean': 'CleanStyle',
+    'red_blue': 'RedBlueStyle',
+    'dark_colorized': 'DarkColorizedStyle',
+    'light_colorized': 'LightColorizedStyle',
+    'turquoise': 'TurquoiseStyle',
+    'light_green': 'LightGreenStyle',
+    'dark_green': 'DarkGreenStyle',
+    'dark_green_blue': 'DarkGreenBlueStyle',
+    'blue': 'BlueStyle',
+}
 
 allowed_interpolations = [
     'cubic',
@@ -21,6 +41,9 @@ class PygalViewMixin(object):
         interpolate = self.request.GET.get('interpolate', None)
         if interpolate and interpolate in allowed_interpolations:
             options['interpolate'] = interpolate
+        style = self.request.GET.get('style', None)
+        if style and style in allowed_styles:
+            options['style'] = pydoc.locate('pygal.style.%s' % allowed_styles[style])
         return options
 
     def get_chart(self):
