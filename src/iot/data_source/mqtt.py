22a6b0c6a44a21b706800b4d94b7f885f78df0c2
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2017-11-20 12:46:58 -0200

    json

--- src/iot/data_source/mqtt.py
+++ src/iot/data_source/mqtt.py
@@ -1,17 +1,33 @@
+import json
 from typing import Iterator
+
+from paho.mqtt import client as mqtt
+
 from iot.models import SensorData
 
 
 class MQTTDataSourceStrategy(object):
-    def parse(self, *args, **kwargs) -> Iterator[SensorData]:
+    def parse(self, mqtt_client: mqtt.Client, userdata, msg) -> Iterator[SensorData]:
         raise NotImplementedError()
 
 
 class JSONDataSourceStrategy(MQTTDataSourceStrategy):
-    def parse(self):
-        pass
+    def __init__(self, encoding='utf-8'):
+        self.encoding = encoding
+        self.mqtt_client = None
+        self.userdata = None
+        self.msg = None
+
+    def parse_data(self, topic, data) -> Iterator[SensorData]:
+        raise NotImplementedError()
+
+    def to_json(self, payload):
+        return json.loads(payload.decode(self.encoding))
+
+    def parse(self, mqtt_client: mqtt.Client, userdata, msg) -> Iterator[SensorData]:
+        data = self.to_json(msg.payload)
+        return self.parse_data(msg.topic, data)
 
 
-class MQTTSingleTopicSensor(object):
-    def __init__(self):
-        pass
+class MQTTSingleTopicSensor(JSONDataSourceStrategy):
+    pass
